2e72675afbb3079bac278402d5c0beda1d25b91f
s1097736 <victor.petre@ru.nl>
2024-11-27 17:50:49 +0100

    Push remote changes for local work

--- experiments/template_experiment/train.py
+++ experiments/template_experiment/train.py
@@ -8,7 +8,6 @@ from decorr_mamba.model.decorrelation import DecorrMamba
 from decorr_mamba.utils.trainer import MambaTrainer
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-# os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 GPU = 6
 
 
@@ -35,7 +34,7 @@ if __name__ == "__main__":
 	print(f"Using device: {device}")
 	mamba_args = MambaArgs(N, D, n_layers=2, device=device, vocab_size=1024)
 
-	print("Creating model...")
+	print(f"Creating model with following properties:\n{mamba_args}\n")
 	decorr_model = DecorrMamba("channel_independent", mamba_args, 
 		sample_frac=0.1, kappa=0.5, decorr_lr=0.002).to(device)
 	print("Model created.")
@@ -43,11 +42,11 @@ if __name__ == "__main__":
 	# defining the training protocol
 	default_train_args = DefaultArgs()
 	train_args = TrainingArgs(
-	    n_epochs=20, L=L, B=B, lr=1*1.5e-3, **default_train_args.lm_args, warmup_epochs=2)
-	print(f"Training with following training arguments:\n{train_args}")
+	    n_epochs=10, L=L, B=B, lr=1*1.5e-3, **default_train_args.lm_args, warmup_epochs=2)
+	print(f"Training with following training arguments:\n{train_args}\n")
 
 	# datasets sent to the device specified in mamba_args by default 
-	datasets = LanguageDatasetMaker(seqs, mamba_args, train_args, total_dataset_frac=0.001,
+	datasets = LanguageDatasetMaker(seqs, mamba_args, train_args, total_dataset_frac=0.2,
 	                                train_split=0.8, val_split=0.2)
 
 	# creating datasets + trainer
