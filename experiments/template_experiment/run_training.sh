764d5a0dafb523583b133fd14eb833a4ec67264d
Petre, V. (Victor) <victor.petre@ru.nl>
2024-11-27 02:51:02 +0100

    Fix conda source for remote machine in template experiment

--- experiments/template_experiment/run_training.sh
+++ experiments/template_experiment/run_training.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-
-source /opt/anaconda3/etc/profile.d/conda.sh
+# source /opt/anaconda3/etc/profile.d/conda.sh this is the macOS local
+source /usr/local/miniconda3/etc/profile.d/conda.sh
 conda activate thesis_env
 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -10,4 +9,7 @@ timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 python train.py > "logs/output_$timestamp.log" 2>&1
 
 conda deactivate
+
+
+
 
