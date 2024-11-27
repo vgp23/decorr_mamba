91623eb8e86db6cf04e07b0f555243e513d9f72c
s1097736 <victor.petre@ru.nl>
2024-11-27 14:20:36 +0100

    Test commit

--- experiments/template_experiment/train.py
+++ experiments/template_experiment/train.py
@@ -8,7 +8,8 @@ from decorr_mamba.model.decorrelation import DecorrMamba
 from decorr_mamba.utils.trainer import MambaTrainer
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-GPU = 0
+# os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+GPU = 6
 
 
 if __name__ == "__main__":
@@ -19,35 +20,35 @@ if __name__ == "__main__":
 	# train on song lyrics dataset for now
 	print("Loading dataset...")
 	with open("../../datasets/kaggle_song_lyrics_dataset/kaggle_song_lyrics_dataset.pkl", "rb") as f:
-	    seqs = pickle.load(f)
+		seqs = pickle.load(f)
 
 	print("Dataset loaded.")
 
 	# inner model dimensionalities and batch size
-	L = 32
-	B = 10
-	D = 16
-	N = 8
+	L = 64
+	B = 128
+
+	D = 256 
+	N = 64
 
 	device = torch.device(f'cuda:{GPU}' if torch.cuda.is_available() else 'cpu')
 	print(f"Using device: {device}")
-	mamba_args = MambaArgs(N, D, n_layers=2, device=device)
+	mamba_args = MambaArgs(N, D, n_layers=2, device=device, vocab_size=2047)
 
 	print("Creating model...")
 	decorr_model = DecorrMamba("channel_independent", mamba_args, 
-		sample_frac=1.0, kappa=0.5, decorr_lr=0.002).to(device)
+		sample_frac=0.1, kappa=0.5, decorr_lr=0.002).to(device)
 	print("Model created.")
-
 
 	# defining the training protocol
 	default_train_args = DefaultArgs()
 	train_args = TrainingArgs(
-	    n_epochs=20, L=L, B=B, lr=1*1.5e-3, **default_train_args.lm_args, warmup_epochs=0)
+	    n_epochs=20, L=L, B=B, lr=1*1.5e-3, **default_train_args.lm_args, warmup_epochs=2)
 	print(f"Training with following training arguments:\n{train_args}")
 
 	# datasets sent to the device specified in mamba_args by default 
-	datasets = LanguageDatasetMaker(seqs, mamba_args, train_args, total_dataset_frac=0.001,
-	                                train_split=0.5, val_split=0.5)
+	datasets = LanguageDatasetMaker(seqs, mamba_args, train_args, total_dataset_frac=0.1,
+	                                train_split=0.8, val_split=0.2)
 
 	# creating datasets + trainer
 	train_loader = DataLoader(datasets.train_set, B, shuffle=True)
