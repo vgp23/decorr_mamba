Begin fixing incorrect updates of backprop parameters

decorr_mamba/decorr_mamba/model/decorrelation.py
@@ -169,6 +169,7 @@ class DecorrLinear(nn.Linear):
 			torch.eye(self.in_features).to(self.weight.device), requires_grad=False)
 
 		self.register_parameter("original_weight", nn.Parameter(self.weight))
+		print(self.original_weight.requires_grad)
 		# delete self.weight, because we'll be overwriting it later
 		del self._parameters["weight"]
 
@@ -265,8 +266,7 @@ class DecorrMamba(MambaLMHeadModel):
 		self.n_decorr_layers = 0 # used for averaging the decorr losses later
 		# used for referencing decorrelation layers directly without looping
 		# over complete model structure
-		self.decorr_layers = nn.ModuleDict()
-		self.decorr_module_id = 0
+		self.decorr_layers = []
 
 		def _create_decorr_matrices(module):
 			''' 
@@ -278,16 +278,12 @@ class DecorrMamba(MambaLMHeadModel):
 				if name == "in_proj" or name == "out_proj" or name == "x_proj":
 					self.n_decorr_layers += 1
 					setattr(module, name, DecorrLinear(original_layer=child))
-					self.decorr_layers[f"{self.decorr_module_id}_{name}"] = \
-						getattr(module, name)
-					self.decorr_module_id +=1
+					self.decorr_layers.append(getattr(module, name))
 
 				if name == "conv1d":
 					self.n_decorr_layers += 1 				
 					setattr(module, name, DecorrConv1d(original_layer=child))
-					self.decorr_layers[f"{self.decorr_module_id}_{name}"] = \
-						getattr(module, name)
-					self.decorr_module_id +=1
+					self.decorr_layers.append(getattr(module, name))
 
 		self.apply(_create_decorr_matrices)
 
@@ -441,8 +437,8 @@ class DecorrMamba(MambaLMHeadModel):
 
 	def apply_to_decorr(self, f):
 		"Used for applying simple functions to all of a model's decorrelated layers"
-		for key in self.decorr_layers.keys():
-			f(self.decorr_layers[key])
+		for layer in self.decorr_layers:
+			f(layer)
 
 
 	

decorr_mamba/decorr_mamba/utils/trainer.py
@@ -148,8 +148,11 @@ class MambaTrainer:
 			if train_backprop:
 				optimizer.zero_grad()
 			
-			if isinstance(self.model, DecorrMamba) and train_decorr:
-				self.model.reset_decorr()
+			# do this regardless of whether we're training these parameters or 
+			# not, because the reset is necessary to compute losses correctly
+			# during a forward pass
+			if isinstance(self.model, DecorrMamba):
+				self.model.reset_decorr(re_fuse=train_decorr)
 			
 			with torch.amp.autocast(self.device.type, enabled=use_amp):
 				# shift input sequence by one token and compare
@@ -169,7 +172,7 @@ class MambaTrainer:
 				# then averaging them across the architecture
 			
 				self.model.reshape_decorr_inputs(b=b)
-				self.model.compute_decorr_grad_loss()				
+				self.model.compute_decorr_grad_loss(compute_grad=train_decorr)		
 				self.model.mean_decorr_losses()		
 					
 				train_corr_loss = self.model.mean_corr_loss.item()